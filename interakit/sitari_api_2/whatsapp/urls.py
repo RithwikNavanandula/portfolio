c7bda023198cf2ba94e5a52ea6ffc5547a02009c
Rithwik Navanandula <140204524+RithwikNavanandula@users.noreply.github.com>
2025-12-29 10:46:04 +0530

    Delete interakit/sitari_api_2 directory

--- interakit/sitari_api_2/whatsapp/urls.py
+++
@@ -1,9 +0,0 @@
-"""
-whatsapp/urls.py - WhatsApp API URLs
-"""
-from django.urls import path
-from .views import WebhookView
-
-urlpatterns = [
-    path('webhook/', WebhookView.as_view(), name='webhook'),
-]
